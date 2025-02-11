8a1c9923e7312591964afd927d2db56772d71b4d
Vigneshwar21 <vigneshwarpandu@gmail.com>
2025-02-11 10:35:11 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -6,11 +6,11 @@ const ThemeToggler=()=>{
   const [theme,setTheme] = useState('light')
 
   const toggleTheme = () =>{
-    setTheme(prevTheme=>(prevTheme == 'light' ? 'dark' : 'light'))
+    setTheme(prevTheme=>(prevTheme === 'light' ? 'dark' : 'light'))
   }
 
   return(
-    <div style={{backgroundColor:theme=='light'?'white':'black' , color:theme=='light'? 'black':'white' ,textAlign:'center'}}>
+    <div style={{backgroundColor:theme==='light'?'white':'green' , color:theme==='light'? 'black':'white' ,textAlign:'center'}}>
       <h1>The Current theme is {theme}</h1>
       <button onClick={toggleTheme}>toggleTheme</button>
     </div>
