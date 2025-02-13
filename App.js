2c1ec6df3cce2c75916716b123540a26e6f7cd59
Vigneshwar21 <vigneshwarpandu@gmail.com>
2025-02-13 10:31:12 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,26 +1,43 @@
-import React,{createContext, useContext}from 'react';
-const DataContext = createContext();
+import React, { createContext, useContext } from "react";
 
-function App(){
-  const data="Hello I am vigneshwar"
-  return(
-    <DataContext.Provider value={data}>
-<User1></User1>
-    </DataContext.Provider>
-  )
+
+const UserContext = createContext();
+
+function App() {
+  const user = {
+    name: "saicharan",
+    email: "charan@gmail.com"
+  };
+
+  return (
+    <UserContext.Provider value={user}>
+      <User1 />
+    </UserContext.Provider>
+  );
 }
-// user1 compomnent
-function User1({data}){
-  return<User2 />
+
+function User1() {
+  return <User2 />;
 }
-function User2({data}){
-  return<User3/>
+
+function User2() {
+  return <User3 />;
 }
-function User3({data}){
-  return<User4 />
+
+function User3() {
+  return <Profile />;
 }
-function User4(){
-  const data=useContext(DataContext)
-  return<div>{data}</div>
+
+
+function Profile() {
+  const user = useContext(UserContext);
+  
+  return (
+    <div>
+     <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+    </div>
+  );
 }
+
 export default App;
